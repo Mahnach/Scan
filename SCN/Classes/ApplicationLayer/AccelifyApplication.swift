264612bf842531dee140b63d8b50ed48fd254fb3
Evgeny <y.makhnach@vironit.com>
2018-04-10 10:36:07 +0300

    bugfixing

---
+++ SCN/Classes/ApplicationLayer/AccelifyApplication.swift
@@ -0,0 +1,9 @@
+//
+//  AccelifyApplication.swift
+//  SCN
+//
+//  Created by BAMFAdmin on 27.02.18.
+//  Copyright © 2018 BAMFAdmin. All rights reserved.
+//
+
+import Foundation
---
+++ SCN/Classes/ApplicationLayer/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  SCN
+//
+//  Created by BAMFAdmin on 27.02.18.
+//  Copyright © 2018 BAMFAdmin. All rights reserved.
+//
+
+import Foundation
